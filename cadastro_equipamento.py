cd9b7d824ad99fca45b99db414782b3b2edec054
Amand95 <amanda.barros@fiap.com.br>
2025-04-22 15:26:28 -0300

    Delete cadastro_equipamento.py

--- cadastro_equipamento.py
+++
@@ -1,7 +0,0 @@
-def cadastrar_equipamento(id_equipamento, tipo, modelo):
-    equipamento = {
-        'id_equipamento': id_equipamento,
-        'tipo': tipo,
-        'modelo': modelo
-    }
-    equipamentos.append(equipamento)
