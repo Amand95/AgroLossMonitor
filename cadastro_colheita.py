75a9d52b8e8bb38121dad7ca5c3538b2cfb4bd95
Amand95 <amanda.barros@fiap.com.br>
2025-04-22 14:44:32 -0300

    Update and rename Estrutura to cadastro_colheita.py

---
+++ cadastro_colheita.py
@@ -0,0 +1,6 @@
+def cadastrar_colheita(local, area):
+    # Aqui você pode incluir a lógica para cadastrar a colheita.
+    # Por exemplo, adicionando esses dados a uma lista ou banco de dados.
+    colheita = {"local": local, "area": area}
+    return colheita
+
